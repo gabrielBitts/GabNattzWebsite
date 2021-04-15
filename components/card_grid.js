0beff5cc92de9980e3bd465510a884f6943c14f8
gabrielBitts <gabrielbits@live.com>
2021-04-15 04:02:25 -0300

    Twitch iframe implemented, solved css inconsistencies, implemented copyright footer, solved responsiveness problems

--- components/card_grid.js
+++ components/card_grid.js
@@ -2,82 +2,36 @@ import React from 'react'
 import Image from 'next/image'
 
 export class Grid extends React.Component {
-    render(){
+    render() {
         return (
-        <div id="grid-body">
-        <div class="card_container">
-            <div class="cards">
-                <div class="cover">
-                    <div class="cover_front">
-                        <Image src="/covers/cards/diablo-2.png" alt="Capa Diablo 2" layout="fill"/>
-                    </div>
-                    <div class="cover_back">
-                        <h1 class="game_name">Diablo II</h1>
-                        <p class="company">Blizzard North</p>
-                        <p class="platform">PC</p>
-                        <p class="year">1999</p>
-                        <p class="data_zeramento">01/02/2021</p>
+            <div id="grid-body">
+                <div className="card_container">
+                    <Card/><Card/><Card/><Card/>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="cards">
+                    <div className="cover">
+                        <div className="cover_front">
+                            <Image src="/covers/cards/diablo-2.png" alt="Capa Diablo 2" layout="fill"/>
+                        </div>
+                        <div className="cover_back">
+                            <h1 className="game_name">Diablo II</h1>
+                            <p className="company">Blizzard North</p>
+                            <p className="platform">PC</p>
+                            <p className="year">1999</p>
+                            <p className="data_zeramento">01/02/2021</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="cards">
-                <div class="cover">
-                    <div class="cover_front">
-                        <Image src="/covers/cards/diablo-2.png" alt="Capa Diablo 2" layout="fill"/>
-                    </div>
-                    <div class="cover_back">
-                        <h1 class="game_name">Diablo II</h1>
-                        <p class="company">Blizzard North</p>
-                        <p class="platform">PC</p>
-                        <p class="year">1999</p>
-                        <p class="data_zeramento">01/02/2021</p>
-                    </div>
-                </div>
-            </div>
-            <div class="cards">
-                <div class="cover">
-                    <div class="cover_front">
-                        <Image src="/covers/cards/diablo-2.png" alt="Capa Diablo 2" layout="fill"/>
-                    </div>
-                    <div class="cover_back">
-                        <h1 class="game_name">Diablo II</h1>
-                        <p class="company">Blizzard North</p>
-                        <p class="platform">PC</p>
-                        <p class="year">1999</p>
-                        <p class="data_zeramento">01/02/2021</p>
-                    </div>
-                </div>
-            </div>
-            <div class="cards">
-                <div class="cover">
-                    <div class="cover_front">
-                        <Image src="/covers/cards/diablo-2.png" alt="Capa Diablo 2" layout="fill"/>
-                    </div>
-                    <div class="cover_back">
-                        <h1 class="game_name">Diablo II</h1>
-                        <p class="company">Blizzard North</p>
-                        <p class="platform">PC</p>
-                        <p class="year">1999</p>
-                        <p class="data_zeramento">01/02/2021</p>
-                    </div>
-                </div>
-            </div>
-            <div class="cards">
-                <div class="cover">
-                    <div class="cover_front">
-                        <Image src="/covers/cards/diablo-2.png" alt="Capa Diablo 2" layout="fill"/>
-                    </div>
-                    <div class="cover_back">
-                        <h1 class="game_name">Diablo II</h1>
-                        <p class="company">Blizzard North</p>
-                        <p class="platform">PC</p>
-                        <p class="year">1999</p>
-                        <p class="data_zeramento">01/02/2021</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
         )
     }
 }
---
+++ components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="info">
+                    <a className="copyright">© Direitos Reservados: </a>
+                    <a className="name">Gabriel Nattis</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer
